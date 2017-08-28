14a5c9803e3a723cf339f7bd8b2b88c6cb4f27cd
Justin Minsk <justin.minsk@gmail.com>
2017-08-28 14:56:45 -0400

    Added inline comments

--- data_Visu.R
+++ data_Visu.R
@@ -1,15 +1,17 @@
-library(tidyverse)
+library(tidyverse) # Library I am using for ggplot2 and rplyer
 
-mpg
+mpg #First set of data
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+  geom_point(mapping = aes(x = displ, y = hwy, color = class)) #Basic point plot with color
+#shows the engine type and highway milage the car gets and also what type of car it is with color
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(mapping = aes(color = class)) +
-  geom_smooth()
+  geom_smooth() #adds a line of best fit showing that highway mileage lowers with the amount of
+#cylinders 
 
-diamonds
+diamonds #second data set
 
 ggplot(data = diamonds) +
-  geom_bar(mapping = aes(x = cut))
+  geom_bar(mapping = aes(x = cut)) #creates a basic bar graph
