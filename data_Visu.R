b45b694fbdc9a92c906d1f6fb7f7c92a3a22cb46
Justin Minsk <justin.minsk@gmail.com>
2017-08-30 08:06:08 -0400

    Added more examples and changed inline comments format

--- data_Visu.R
+++ data_Visu.R
@@ -1,17 +1,25 @@
-library(tidyverse) # Library I am using for ggplot2 and rplyer
+library(tidyverse) # Library I am using for ggplot2 and dplyr
 
 mpg #First set of data
 
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy, color = class)) #Basic point plot with color
-#shows the engine type and highway milage the car gets and also what type of car it is with color
+  geom_point(mapping = aes(x = displ, y = hwy, color = class)) 
+#Basic point plot with color shows the engine type and highway milage the car gets and also what type of car it is with color
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(mapping = aes(color = class)) +
-  geom_smooth() #adds a line of best fit showing that highway mileage lowers with the amount of
-#cylinders 
+  geom_smooth() 
+#adds a line of best fit showing that highway mileage lowers with the amount of cylinders 
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ class, nrow = 2)
+#breaks data into different graphs based on the class of car
+
+#second chapter
 
 diamonds #second data set
 
 ggplot(data = diamonds) +
   geom_bar(mapping = aes(x = cut)) #creates a basic bar graph
+
