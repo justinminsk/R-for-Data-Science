1f8e73b672dddddc522d0823582ac498a0a6571e
Justin Minsk <justin.minsk@gmail.com>
2017-08-30 09:28:03 -0400

    finished chapter one added examples from chapter two

--- data_Visu.R
+++ data_Visu.R
@@ -16,10 +16,57 @@ ggplot(data = mpg) +
   facet_wrap(~ class, nrow = 2)
 #breaks data into different graphs based on the class of car
 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"),
+    se = FALSE
+  )
+#the smooth line now only is based off of the subcompact car class
+
 #second chapter
+library(tidyverse)#need to load this every time
 
 diamonds #second data set
 
 ggplot(data = diamonds) +
-  geom_bar(mapping = aes(x = cut)) #creates a basic bar graph
+  geom_bar(mapping = aes(x = cut)) 
+#creates a basic bar graph
+
+ggplot(data = diamond) +
+  stat_count(maping = aes(x = cut))
+#makes the same bar graph different code
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = ..prop.., group = 1))
+#makes a bar graph of proportions rather than bulk numbers
+
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median
+  )
+#makes a box graph here comparing cut and depth they are found at
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, color = cut, fill = cut))
+#color adds an outline, fill fills thebars a color
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity))
+#if you use a second varible will fill the bars in proportion to the bar, here cut broken down into clarity
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity),
+  position = "fill"
+  )
+#makes the bars even but shows proportion of cut compared to clarity better
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity),
+           position = "dodge"
+  )
+#makes seprate bars to better show numbers over proportions
 
