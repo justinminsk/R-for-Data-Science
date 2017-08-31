e03731ef67f5909123e946020e67330f038cbf0f
Justin Minsk <justin.minsk@gmail.com>
2017-08-30 20:38:24 -0400

    Changed chapter3.R to make it up to date

--- Chapter_3.R
+++ Chapter_3.R
@@ -33,3 +33,31 @@ arrange(flights, desc(arr_delay))
 select(flights, year, month, day)
 #selects only certain columns, can use for example (year:day) all columns between two columns or -(year:day) to
 #selects everything but the columns you specifiy in this example day, month, and year
+#select also uses common string functions like contains, starts_with, etc.
+
+flights_sml <- select(flights, year:day, ends_with("delay"), distance, air_time)
+#makes a sample set of day, month, year, all delay sets, distance, and air time
+
+mutate(flights_sml, gain = arr_delay - dep_delay, speed = distance / air_time * 60)
+#creates columns for gain and speed based off of our parameters
+
+transmute(flights, gain = arr_delay - dep_delay, hours = air_time / 60, gain_per_hour = gain/hours )
+#only keeps new varibles that you create using your parameters
+
+x <- 1:10
+#make x 1-10
+lag(x)
+#looks at x from one spot the the left so NA to 9 instead
+lead(x)
+#looks at x from one spot the the right so 2 to NA instead
+cumsum(x)
+#adds all values
+cummean(x)
+#finds rolling mean
+min_rank(x)
+#ranks from smallest to largest 
+
+by_day <- group_by(flights, year, month, day)
+#group by_day in flights by year, month, day
+summarise(by_day, delay = mean(dep_delay, na.rm = TRUE))
+#finds the mean delay for a day exculding NA's
